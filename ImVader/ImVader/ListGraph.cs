a8e94d55adcbdfcb2eb50cfa795f287cbe59761b
Konnor 95 <dima95kh@gmail.com>
2014-09-28 15:25:55 +0300

    Untracked files added

---
+++ ImVader/ImVader/ListGraph.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ListGraph.cs" company="Sigma">
+//    I have no idea what should be written here.
+// </copyright>
+// <summary>
+//   The list graph.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImVader
+{
+    /// <summary>
+    /// The list graph.
+    /// </summary>
+    public class ListGraph : IGraph
+    {
+        /// <summary>
+        /// The topological sort.
+        /// </summary>
+        public void TopologicalSort()
+        {
+        }
+    }
+}
---
+++ ImVader/ImVader/MatrixGraph.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MatrixGraph.cs" company="Sigma">
+//   I have no idea what should be written here.
+// </copyright>
+// <summary>
+//   Defines the MatrixGraph type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImVader
+{
+    /// <summary>
+    /// The matrix graph.
+    /// </summary>
+    public class MatrixGraph : IGraph
+    {
+        /// <summary>
+        /// The topological sort.
+        /// </summary>
+        public void TopologicalSort()
+        {
+        }
+    }
+}
