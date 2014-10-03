0e33125926933e997b15c17aadc49eef9eb4435b
Konnor95 <dima95kh@gmail.com>
2014-10-04 00:03:23 +0300

    Revert "Demo page added"
    This reverts commit b4e62988d34bf188ce00ab10cfc9a268c0c1fee1.

--- ImVader/ImVaderWebsite/Controllers/HomeController.cs
+++ ImVader/ImVaderWebsite/Controllers/HomeController.cs
@@ -6,8 +6,6 @@ using System.Web.Mvc;
 
 namespace ImVaderWebsite.Controllers
 {
-    using System.Web.Http;
-
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -21,15 +19,11 @@ namespace ImVaderWebsite.Controllers
 
             return View();
         }
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
-
-        public ActionResult Demo()
-        {
             return View();
         }
     }
