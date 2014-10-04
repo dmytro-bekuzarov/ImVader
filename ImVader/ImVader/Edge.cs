d9f272075e55a1d8df5acebd296b8d7f0212d10f
Konnor95 <dima95kh@gmail.com>
2014-10-04 15:20:37 +0300

    Commit fix

---
+++ ImVader/ImVader/Edge.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Edge.cs" company="Sigma">
+//   Copyright
+// </copyright>
+// <summary>
+//   Defines the Edge type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ImVader
+{
+    using System;
+
+    public class Edge
+    {
+        private readonly IVertex v;
+        private readonly IVertex w;
+
+        public Edge(IVertex v, IVertex w)
+        {
+            this.v = v;
+            this.w = w;
+        }
+
+        public IVertex Either()
+        {
+            return v;
+        }
+
+        /// <summary>
+        /// The other.
+        /// </summary>
+        /// <param name="vertex">
+        /// The vertex.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IVertex"/>.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        /// </exception>
+        public IVertex Other(IVertex vertex)
+        {
+            if (vertex == v) return w;
+            if (vertex == w) return v;
+            throw new ArgumentException();
+        }
+    }
+}
---
+++ ImVader/ImVaderWebsite/VKConnection/script/vkConnection.js
@@ -0,0 +1,43 @@
+/**
+ * Created by JetBrains PhpStorm.
+ * User: bebecap
+ * Date: 03.10.14
+ * Time: 20:44
+ * To change this template use File | Settings | File Templates.
+ */
+function onPageLoaded()
+{
+    console.log("Page loaded");
+    VK.init({
+        apiId: 4575060
+    });
+    VK.Auth.getLoginStatus(authInfo);
+    VK.UI.button('login_button');
+}
+function authInfo(response) {
+  if (response.session) {
+    //Hiding vk auth button
+     $("#login_button").addClass("hidden");
+     $("#vk_greetings").removeClass("hidden");
+     $("#vk_friends_json").removeClass("hidden");
+
+   //Getting user info
+    console.log('user: '+response.session.mid);
+    VK.Api.call('users.get', {uids: response.session.mid}, function(r) {
+        if(r.response) {
+            $("#vk_greetings").text("Hello, "+r.response[0].first_name+". Here are your friend list in JSON.");
+        }
+    });
+    //Getting user friends
+    VK.Api.call('friends.get', {user_id:response.session.mid, fields: "photo_200_orig"}, function (r)
+    {
+        if (r.response)
+        {
+            console.log(r);
+            $("#vk_friends_json").text(JSON.stringify(r.response));
+        }
+    });
+  } else {
+    console.log('not auth');
+  }
+}
