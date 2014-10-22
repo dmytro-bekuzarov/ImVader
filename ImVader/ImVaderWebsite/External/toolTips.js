e5f692f7641b5cf6185be670d758f26b0b5b9acd
Moodok <diablosmelnik@yandex.ru>
2014-10-22 16:49:05 +0300

    Tooltips and index page

---
+++ ImVader/ImVaderWebsite/External/toolTips.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $('#getFriendsButton').tooltip({
+        'placement': 'right',
+        'title': "Click here to show user's friends"
+    });
+
+    $('#dijkstra').tooltip({
+        'placement': 'right',
+        'title': "Click here to find path between friends"
+    });
+
+    $('#mst').tooltip({
+        'placement': 'right',
+        'title': "Click here to build mst of the graph"
+    });
+
+    $('#saveload').tooltip({
+        'placement': 'right',
+        'title': "Click here to save graph to .txt file or load the graph from .txt file"
+    });
+
+    $("#myModal").modal("show");
+});
