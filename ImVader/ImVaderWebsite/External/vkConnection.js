f1058d8ff1c21cfb3ac402ca01912429f20fb685
Konnor95 <dima95kh@gmail.com>
2014-10-28 22:07:06 +0200

    Loading VK friends fixed

--- ImVader/ImVaderWebsite/External/vkConnection.js
+++ ImVader/ImVaderWebsite/External/vkConnection.js
@@ -2,7 +2,7 @@ var user_id = null;
 var offset = 0;
 var offsetArray = new Array();
 var friends = null;
-var counter=0;
+var counter = 0;
 function initializeVk() {
     console.log("Page loaded");
     VK.init({
@@ -10,7 +10,7 @@ function initializeVk() {
     });
     VK.Auth.getLoginStatus(authInfo);
     VK.UI.button('login_button');
-    VK.Auth.login(authInfo,2);
+    VK.Auth.login(authInfo, 2);
 }
 function authInfo(response) {
     if (response.session) {
@@ -40,20 +40,52 @@ function getFriends() {
             if (!offsetArray[user_id]) {
                 offsetArray[user_id] = 0;
             }
-            VK.Api.call('friends.get', { user_id: user_id, fields: "photo_200_orig,sex,counters", offset: offsetArray[user_id], count: 5 }, function (r) {
-                if (r.response) {
-                    offsetArray[user_id] += 5;
-                    time = r.response.length;
-                    for (var i = 0; i < r.response.length; i++) {
-                        if (!isAdded(r.response[i].uid)) {
-                            addNode(r.response[i]);
+            /* VK.Api.call('friends.get', { user_id: user_id, fields: "photo_200_orig,sex,counters", offset: offsetArray[user_id], count: 5 }, function (r) {
+                 if (r.response) {
+                     offsetArray[user_id] += 5;
+                     time = r.response.length;
+                     for (var i = 0; i < r.response.length; i++) {
+                         if (!isAdded(r.response[i].uid)) {
+                             addNode(r.response[i]);
+                         }
+                         getMutual(user_id, r.response[i].uid);
+                     }
+                     //Value - вес ребра
+                 }
+                 stopSpinner(time);
+             }, function () { stopSpinner(time); });
+             */
+
+
+            var code = 'var friends = API.friends.get({ "user_id": ' + parseInt(user_id) + ', "fields": "photo_200_orig,sex", "offset": ' + offsetArray[user_id] + ', "count": 20 });\n' +
+                'var i = 0;\n var obj = [];\n' +
+                'while (i != parseInt(friends.length) && i <= 20) {\n' +
+                'var mutual = API.friends.getMutual({ "source_uid": ' + parseInt(user_id) + ', "target_uid": friends[i].uid });\n' +
+                'obj.push({ "uid": friends[i].uid, "photo_200_orig": friends[i].photo_200_orig, "sex" : friends[i].sex, "first_name": friends[i].first_name, "last_name": friends[i].last_name, "count": mutual.length });\n' +
+                'i = i + 1;\n }\n' +
+                'return obj;';
+            console.log(code);
+            VK.api('execute', {
+                code: code
+            }, function (resp) {
+                resp = resp.response;
+                if (resp) {
+                    offsetArray[user_id] += 20;
+                    time = resp.length;
+                    for (var i = 0; i < resp.length; i++) {
+                        if (resp[i].count != 0) {
+                            if (!isAdded(resp[i].uid)) {
+                                addNode(resp[i]);
+                            }
+                            if (!hasEdge(user_id, resp[i].uid))
+                                addEdge(user_id, resp[i].uid, resp[i].count);
                         }
-                        getMutual(user_id, r.response[i].uid);
                     }
-                    //Value - вес ребра
                 }
+                console.log(resp);
                 stopSpinner(time);
-            }, function () { stopSpinner(time); });
+                console.log(data.nodes);
+            });
         } else {
             console.log('already has friends');
         }
