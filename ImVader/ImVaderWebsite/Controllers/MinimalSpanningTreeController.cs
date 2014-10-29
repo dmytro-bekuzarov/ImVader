8d608cc4a9c750a092f43037a7dcf47a7be35393
ghoulbahor <94baho94@mail.ru>
2014-10-30 00:39:49 +0200

    MST controller fix

--- ImVader/ImVaderWebsite/Controllers/MinimalSpanningTreeController.cs
+++ ImVader/ImVaderWebsite/Controllers/MinimalSpanningTreeController.cs
@@ -1,10 +1,13 @@
 ﻿namespace ImVaderWebsite.Controllers
 {
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
     using System.Web.Http;
-
-    using ImVader;
+    using System.Web.Mvc;
     using ImVader.Algorithms;
+    using ImVader;
 
     public class MinimalSpanningTreeController : ApiController
     {
@@ -15,13 +18,16 @@
 
             public IEnumerable<WeightedEdge> Edges;
         }
-        public int[][] Post([FromBody]SGraph g)
+        public List<WeightedEdge> Post([FromBody]SGraph g)
         {
-            var graph = new ListGraph<int, WeightedEdge>();
-            graph.Init(g.Edges, g.Vertices);
+            var graph = new DirectedListGraph<int, WeightedEdge>(g.Vertices.Count());
+            foreach (var edge in g.Edges)
+            {
+                graph.AddEdge(edge);
+            }
             var mst = new MinimalSpanningTree<int, WeightedEdge>(graph);
-            var mstvert = mst.GetMstVertices();
-            return mstvert;
+            var mstEdges = mst.GetMstEdges();
+            return mstEdges;
         }
 
     }
