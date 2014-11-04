24b031ee55395dc18dadd6d37903c894f13319e3
Konnor95 <dima95kh@gmail.com>
2014-11-04 20:13:05 +0200

    Trash files removed, bundles added

--- ImVader/ImVaderWebsite/App_Start/BundleConfig.cs
+++ ImVader/ImVaderWebsite/App_Start/BundleConfig.cs
@@ -1,15 +1,26 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace ImVaderWebsite
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            
             BundleTable.EnableOptimizations = true;
+            bundles.Add(new ScriptBundle("~/bundles/scripts/main").Include(
+                "~/External/jquery/jquery-migrate-1.2.1.min.js",
+                "~/External/bootstrap/js/bootstrap.min.js",
+                "~/External/gozha-nav/jquery.mobile.menu.js",
+                "~/External/modernizr/modernizr.custom.91224.js",
+                "~/External/colorbox/jquery.colorbox.js",
+                "~/External/jquery-cookie/jquery.cookie.js",
+                "~/External/custom.js"));
+            bundles.Add(new ScriptBundle("~/bundles/scripts/index").Include(
+                "~/External/jquery-event/jquery.event.drag-2.2.js",
+                "~/External/jquery-easing/jquery.easing.1.3.js",
+                "~/External/jquery-easing/jquery.easing.1.3.js",
+                "~/External/jquery-roundabout/jquery.roundabout.min.js",
+                "~/External/swiper/idangerous.swiper.min.js"));
         }
     }
 }
